bcc9db7fc3995d094cedca836d0c7bdf79c70320
wolfxanta <72651107+wolfxanta@users.noreply.github.com>
2020-10-26 19:32:03 +0300

    Create cdortislem.c

---
+++ cdortislem.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+
+int x,y,toplam,carpim,bolum,bolumKalan;
+    
+    printf("İlk sayiyi giriniz: ");
+    scanf("%d",&x);
+    printf("İkinci sayiyi giriniz:");
+    scanf("%d",&y);
+    
+    toplam = x + y;
+    carpim = x * y;
+    bolum = x / y;
+    bolumKalan = x % y;
+    
+    float bolum = (float)sayi1 / (float)sayi2;   //Küsüratlı işlemlerde format değişikliği
+
+    printf("Girdiğiniz iki sayinin toplamı : %d \n", toplam);
+    printf("Girdiğiniz iki sayinin carpimi : %d \n", carpim);
+    printf("Girdiğiniz iki sayinin birbirine bolumu : %d \n", bolum);
+    printf("Girdiğiniz iki sayinin birbirine bölümünden kalan : %d \n", bolumKalan);
+
+}
